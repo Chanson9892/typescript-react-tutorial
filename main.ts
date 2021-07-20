352689be8eb716150e3b1452c2a31db6bf00e67b
Chanson9892 <chan.hanson5@gmail.com>
2021-07-20 14:23:58 -0700

    can group the methods for the point into a class since they are highly related

--- main.ts
+++ main.ts
@@ -1,10 +1,26 @@
-interface Point{ // we use interface to define the shape of an object.
-    x: number,
+// interface Point{ // we use interface to define the shape of an object.
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+
+class Point{ // x, y are fields for storing data, draw is a function
+    x: number
     y: number
+    draw() {
+        //.....
+    }
+    getDistance(another: Point){ // when function is part of a class we call it a method.
+        // ...
+    }
 }
 
 let drawPoint = (point: Point) => { // can run into problem with too many parameters. encapsulate them into object
     //.... 
+}
+
+let getDistance = (pointA: Point, pointB: Point) => {
+    // .....
 }
 
 drawPoint({
