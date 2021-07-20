5e0b213191fa8e4b01a091ce9759aa137762d70e
Chanson9892 <chan.hanson5@gmail.com>
2021-07-20 14:09:49 -0700

    inline notation for custom objects

---
+++ main.js
@@ -0,0 +1,7 @@
+var drawPoint = function (point) {
+    //.... 
+};
+drawPoint({
+    x: 2,
+    y: 5
+});
---
+++ main.ts
@@ -0,0 +1,8 @@
+let drawPoint = (point: { x: number, y: number }) => { // can run into problem with too many parameters. encapsulate them into object
+    //.... 
+}
+
+drawPoint({
+    x: 2,
+    y: 5
+})
